07c4e2fef5b78d25dbac340c8f3f5786bb113647
Felix Jeckel <felix.jeckel@akelius.de>
2020-11-02 19:51:06 +0100

    adding social icons

--- config.js
+++ config.js
@@ -1,35 +1,42 @@
 module.exports = {
-  siteTitle: 'Felix Jeckel | Portfolio', // <title>
-  manifestName: 'Portfolio',
-  manifestShortName: 'Portfolio', // max 12 characters
-  manifestStartUrl: '/',
-  manifestBackgroundColor: '#663399',
-  manifestThemeColor: '#663399',
-  manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/profile.jpg',
+  siteTitle: "Felix Jeckel | Portfolio", // <title>
+  manifestName: "Portfolio",
+  manifestShortName: "Portfolio", // max 12 characters
+  manifestStartUrl: "/",
+  manifestBackgroundColor: "#663399",
+  manifestThemeColor: "#663399",
+  manifestDisplay: "standalone",
+  manifestIcon: "src/assets/img/profile.jpg",
   pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
-  heading: 'Felix Jeckel',
-  subHeading: 'Web & App Developer.',
-  subHeadingLine2: 'Skills in React, React Native, Javascript, Gatsby, Jest, HTML, CSS.',
+  heading: "Felix Jeckel",
+  subHeading: "Web & App Developer.",
+  subHeadingLine2:
+    "Skills in React, React Native, Javascript, Gatsby, Jest, HTML, CSS.",
   // social
   socialLinks: [
     {
-      style: 'brands',
-      icon: 'fa-github',
-      name: 'Github',
-      url: 'https://github.com/watnFoehn',
+      style: "brands",
+      icon: "fa-github",
+      name: "Github",
+      url: "https://github.com/watnFoehn",
     },
-    /* {
-      style: 'brands',
-      icon: 'fa-twitter',
-      name: 'Twitter',
-      url: 'https://twitter.com/onlyanubhav',
-    }, */
     {
-      style: 'solid',
-      icon: 'fa-envelope',
-      name: 'Email',
-      url: 'mailto:felix.jeckel@mailbox.org',
+      style: "brands",
+      icon: "fa-linkedin",
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/felix-hoehne/",
+    },
+    {
+      style: "brands",
+      icon: "fa-xing",
+      name: "XING",
+      url: "https://www.xing.com/profile/Felix_Jeckel2/cv",
+    },
+    {
+      style: "solid",
+      icon: "fa-envelope",
+      name: "Email",
+      url: "mailto:felix.jeckel@mailbox.org",
     },
   ],
 };
